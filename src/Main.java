22d77d5ee7a1586d6676b6cac31ba8c9e10ee6a0
Munera0323 <muneragarcia123@gmail.com>
2025-04-09 21:16:41 -0500

    Se uso el switch como etiqueta de condicional multiple

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Scanner leer=new Scanner(System.in);
 
     /*DESARROLLAR UN SISTEMA DE NOTAS PARA UN ESTUDIANTE QUE DESEA SABER SU NOTA FINAL(DE CADA MATERIA)
     EL SISTEMA DEBE PEDIR EL NOMBRE DEL ESTUDIANTE, EL DOCUMENTO Y EL PROGRAMA AL QUE ESTA MATRICULADO
@@ -20,7 +22,39 @@ public class Main {
     SI EL ESTUDIANTE HACE PLAN DE MEJORAMIENTO, LA NOTA MAXIMA DE LA MATERIA SERA 3
     EL SISTEMA DEBE DISCRIMINAR LA INFORMACION GENERAL DEL ESTUDIANTE: NOMBRE ESTUDIANTE, NOTA Y MATERIA*/
 
+System.out.println("SISTEMA DE NOTAS");
+System.out.println("Ingrese el nombre de estudiante");
+        String nombreEstudiante= leer.nextLine();
+ System.out.println("Ingresa el documento del estudiante");
+        String documentoEstudiante= leer.nextLine();
+        System.out.println("Selccione el programa: \n1- Desarrollo de software \n2- Diseño grafico \n3- Gastronomia");
+             /* "1 - " para poder identificar una variable y hacer que sea mas facil*/
 
+        String programa= leer.nextLine();
+        switch (programa) {                  /* switch: etiqueta para condicional multiple*/
+            case "1" :
+                System.out.println("Programa de Desarrollo de software");
+                System.out.println("Seleccione la materia: \n1- Metodologias Agiles \n2- Logica de programacion \n3- Introduccion a la programacion");
+                  String materia= leer.nextLine();
+                   switch (materia) {
+                       case "1":
+                           System.out.println(" NOTA Metodologias Agiles");
+                           break;
+                       case "2":
+                           System.out.println("NOTA Logica de programacion");
+                           break;
+                       case "3":
+                           System.out.println("NOTA Introduccion a la rogramacion");
+                           break;
+                   }
+                break;
+            case "2":
+                System.out.println("Programa de Diseño grafico");
+                break;
+            case "3":
+                System.out.println("Programa de Gastronomia");
+                break;
 
+        }
     }
 }
